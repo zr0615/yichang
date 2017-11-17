94ad427aeed955f43c2bc3eef3fbfeb7926bc1e0
刘春鲜 <139402260@qq.com>
2017-11-17 11:09:06 +0800

    '2017-11-17-11:08'

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// css style
+import './style/common.scss'
+
+// 设置为 false 以阻止 vue 在启动时生成生产提示。
 Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
